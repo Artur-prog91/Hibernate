00225fdcea46cf2517cacc858ebd2df323e8f748
Артур <titeeek2@mail.ru>
2025-06-22 19:40:15 +0300

    first commit

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,34 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+public class Main {
+    public static void main(String[] args) {
+
+        UserService userService = new UserServiceImpl();
+
+        userService.dropUsersTable();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Artur", "Shabaev", (byte)33);
+        System.out.println("Пользователь с именем Artur добавлен в базу данных");
+        userService.saveUser("Ivan", "Ivanov", (byte)42);
+        System.out.println("Пользователь с именем Ivan добавлен в базу данных");
+        userService.saveUser("Petr", "Petrov", (byte)25);
+        System.out.println("Пользователь с именем Petr добавлен в базу данных");
+        userService.saveUser("Andrey", "Seregin", (byte)30);
+        System.out.println("Пользователь с именем Andrey добавлен в базу данных");
+
+        userService.removeUserById(2);
+
+        System.out.println(userService.getAllUsers());
+
+        userService.cleanUsersTable();
+
+        //userService.dropUsersTable();
+
+    }
+}
---
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -0,0 +1,123 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+
+import java.util.List;
+
+public class UserDaoHibernateImpl implements UserDao {
+
+    private SessionFactory sessionFactory = Util.getSessionFactory();
+
+    private static final String CREATE_SQL = "CREATE TABLE IF NOT EXISTS mydbtest.users (id INT NOT NULL AUTO_INCREMENT, " +
+                                             "name VARCHAR(50) NOT NULL, " + "last_name VARCHAR(50) NOT NULL, " +
+                                             "age TINYINT NOT NULL, " + "PRIMARY KEY (id))";
+    private static final String DROP_SQL = "DROP TABLE IF EXISTS users";
+
+    public UserDaoHibernateImpl() {
+
+    }
+
+    @Override
+    public void createUsersTable() {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = null;
+            try {
+                transaction = session.beginTransaction();
+                session.createNativeQuery(CREATE_SQL).executeUpdate();
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void dropUsersTable() {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = null;
+            try {
+                transaction = session.beginTransaction();
+                session.createNativeQuery(DROP_SQL).executeUpdate();
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void saveUser(String name, String lastName, byte age) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = null;
+            try {
+                transaction = session.beginTransaction();
+                User user = new User(name, lastName, age);
+                session.save(user);
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void removeUserById(long id) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = null;
+            try {
+                transaction = session.beginTransaction();
+                User user = session.get(User.class, id);
+                session.delete(user);
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        try (Session session = sessionFactory.openSession()) {
+            List<User> user= session.createQuery("FROM User", User.class)
+                    .getResultList();
+            return user;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void cleanUsersTable() {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = null;
+            try {
+                transaction = session.beginTransaction();
+
+                session.createNativeQuery("TRUNCATE TABLE users").executeUpdate();
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
+            }
+        }
+    }
+}
