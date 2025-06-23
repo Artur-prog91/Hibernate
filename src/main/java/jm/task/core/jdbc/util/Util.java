39c92f224ac42ab60059313a7cbd784f8007c2e4
Артур <titeeek2@mail.ru>
2025-06-23 22:09:55 +0300

    two commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,12 +3,30 @@ package jm.task.core.jdbc.util;
 
 import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
 
 public class Util {
-    private static Configuration configuration = new Configuration().addAnnotatedClass(User.class);
 
-    public static SessionFactory getSessionFactory() {
-        return configuration.configure().buildSessionFactory();
+    private SessionFactory sessionFactory;
+
+    public SessionFactory getSessionFactory() {
+        Configuration configuration = new Configuration();
+        Properties properties = new Properties();
+        properties.put("hibernate.connection.url", "jdbc:mysql://localhost:3306/mydbtest");
+        properties.put("hibernate.connection.username", "root");
+        properties.put("hibernate.connection.password", "root");
+        properties.put("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.hbm2ddl_auto", "update");
+
+        configuration.setProperties(properties).addAnnotatedClass(User.class);
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(properties).build();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        return sessionFactory;
     }
 }
