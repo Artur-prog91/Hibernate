adc91d77601877dd318bf939d80291b7a1fe8dfb
Артур <titeeek2@mail.ru>
2025-06-24 19:56:48 +0300

    three commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -12,7 +12,6 @@ import java.util.List;
 public class UserDaoHibernateImpl implements UserDao {
 
     private SessionFactory sessionFactory = new Util().getSessionFactory();
-    Transaction transaction = null;
     private static final String CREATE_SQL = "CREATE TABLE IF NOT EXISTS mydbtest.users (id INT NOT NULL AUTO_INCREMENT, " +
                                              "name VARCHAR(50) NOT NULL, " + "last_name VARCHAR(50) NOT NULL, " +
                                              "age TINYINT NOT NULL, " + "PRIMARY KEY (id))";
@@ -25,7 +24,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void createUsersTable() {
         try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
+            Transaction transaction = session.beginTransaction();
             session.createNativeQuery(CREATE_SQL).executeUpdate();
             transaction.commit();
         } catch (Exception e) {
@@ -36,7 +35,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void dropUsersTable() {
         try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
+            Transaction transaction = session.beginTransaction();
             session.createNativeQuery(DROP_SQL).executeUpdate();
             transaction.commit();
         } catch (Exception e) {
@@ -46,6 +45,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             User user = new User(name, lastName, age);
@@ -61,6 +61,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void removeUserById(long id) {
+        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             User user = session.get(User.class, id);
@@ -88,7 +89,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void cleanUsersTable() {
         try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
+            Transaction transaction = session.beginTransaction();
             session.createNativeQuery("TRUNCATE TABLE users").executeUpdate();
             transaction.commit();
         } catch (Exception e) {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -14,19 +14,25 @@ public class Util {
     private SessionFactory sessionFactory;
 
     public SessionFactory getSessionFactory() {
-        Configuration configuration = new Configuration();
-        Properties properties = new Properties();
-        properties.put("hibernate.connection.url", "jdbc:mysql://localhost:3306/mydbtest");
-        properties.put("hibernate.connection.username", "root");
-        properties.put("hibernate.connection.password", "root");
-        properties.put("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
-        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-        properties.put("hibernate.show_sql", "true");
-        properties.put("hibernate.hbm2ddl_auto", "update");
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties properties = new Properties();
+                properties.put("hibernate.connection.url", "jdbc:mysql://localhost:3306/mydbtest");
+                properties.put("hibernate.connection.username", "root");
+                properties.put("hibernate.connection.password", "root");
+                properties.put("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+                properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+                properties.put("hibernate.show_sql", "true");
+                properties.put("hibernate.hbm2ddl_auto", "update");
 
-        configuration.setProperties(properties).addAnnotatedClass(User.class);
-        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(properties).build();
-        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                configuration.setProperties(properties).addAnnotatedClass(User.class);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(properties).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         return sessionFactory;
     }
 }
